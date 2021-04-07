69279132d19d8f15240b33fce4b918b3eea5a1db
pdl39 <dhlee9111@gmail.com>
2021-04-07 17:56:47 +0900

    [1-array] ADD: problem no. 5

--- src/1-arrays/5-non-constructible-change.js
+++ src/1-arrays/5-non-constructible-change.js
@@ -1,4 +1,15 @@
-//
+// T: O(nlogn) | S: O(1), if sorted in place
 function nonConstructibleChange(coins) {
-   
+  if (coins.length < 1) return 1;
+  coins.sort((a, b) => a - b);
+
+  let currentMaxChange = 0;
+  for (let i = 0; i < coins.length; i++) {
+    if (coins[i] > currentMaxChange + 1) return currentMaxChange + 1;
+    else currentMaxChange += coins[i];
+  }
+
+  return currentMaxChange + 1;
 }
+
+console.log(nonConstructibleChange([5, 7, 1, 1, 2, 3, 22]));
