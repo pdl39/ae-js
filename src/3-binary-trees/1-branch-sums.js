32a00329003cc17658add67696679b5c45391be7
pdl39 <dhlee9111@gmail.com>
2021-04-09 02:39:23 +0900

    [3-binary-trees] ADD: problem 1

--- src/3-binary-trees/1-branch-sums.js
+++ src/3-binary-trees/1-branch-sums.js
@@ -1,3 +1,18 @@
+
+// T: O(n) | S: O(h) where h = height of the binary tree.
 function branchSums(root) {
-  
+  return branchSumsHelper(root, 0, []);
+}
+
+function branchSumsHelper(tree, sum, sumsArr) {
+  if (!tree) return false;
+
+  let updatedSum = sum + tree.value;
+  let hasLeftChild = branchSumsHelper(tree.left, updatedSum, sumsArr);
+  let hasRightChild = branchSumsHelper(tree.right, updatedSum, sumsArr);
+  if (!hasLeftChild && !hasRightChild) {
+    sumsArr.push(updatedSum);
+  }
+
+  return sumsArr;
 }
