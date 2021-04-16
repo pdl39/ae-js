52da35a55515c98dbe8d95d667704d543eb8583c
pdl39 <dhlee9111@gmail.com>
2021-04-17 03:16:20 +0900

    Revert "[1-arrays] UPDATE: problem 1 recursion method added"
    This reverts commit 4f5a882241196b81a4a5da4b9422e3d64306bf90.

--- src/1-arrays/10-spiral-traverse.js
+++ src/1-arrays/10-spiral-traverse.js
@@ -1,40 +1,37 @@
-// Recursive
-// T: O(n*m) | S: O(n*m)
-// where n = array length and m = nested array length
+// // Recursive
 
 
-function spiralTraverseR(array) {
-    const result = [];
+// function spiralTraverseR(array) {
+//     const result = [];
 
-    let startRow = 0;
-    let endRow = array.length - 1;
-    let startCol = 0;
-    let endCol = array[0].length - 1;
+//     let startRow = 0;
+//     let endRow = array.length - 1;
+//     let startCol = 0;
+//     let endCol = array[0].length - 1;
 
-    spiralTraverseHelper(array, startRow, endRow, startCol, endCol, result);
+//     if (startRow <= endRow && startCol <= endCol) {
+//         spiralTraverseHelper(array, startRow + 1, endRow - 1, startCol + 1, endCol - 1, result);
+//     }
 
-    return result;
-}
+//     return result;
+// }
 
-function spiralTraverseHelper(array, startRow, endRow, startCol, endCol, result) {
-    if (startRow <= endRow && startCol <= endCol) {
-        for (let col = startCol; col <= endCol; col++) {
-            result.push(array[startRow][col]);
-        }
-        for (let row = startRow + 1; row < endRow; row++) {
-            result.push(array[row][endCol]);
-        }
-        for (let col = endCol; col >= startCol; col--) {
-            if (startRow === endRow) break;
-            result.push(array[endRow][col]);
-        }
-        for (let row = endRow - 1; row > startRow; row--) {
-            if (startCol === endCol) break;
-            result.push(array[row][startCol]);
-        }
-        spiralTraverseHelper(array, startRow + 1, endRow - 1, startCol + 1, endCol - 1, result);
-    }
-} 
+// function spiralTraverseHelper(array, startRow, endRow, startCol, endCol, result) {
+//     for (let col = startCol; col <= endCol; col++) {
+//         result.push(array[startRow][col]);
+//     }
+//     for (let row = startRow + 1; row < endRow; row++) {
+//         result.push(array[row][endCol]);
+//     }
+//     for (let col = endCol; col >= startCol; col--) {
+//         if (startRow === endRow) break;
+//         result.push(array[endRow][col]);
+//     }
+//     for (let row = endRow - 1; row > startRow; row--) {
+//         if (startCol === endCol) break;
+//         result.push(array[row][startCol]);
+//     }
+// } 
 
 
 // Iterative
@@ -102,6 +99,6 @@ console.log(spiralTraverse(arr1));
 console.log(spiralTraverse(arr2));
 console.log(spiralTraverse(arr3));
 
-console.log(spiralTraverseR(arr1));
-console.log(spiralTraverseR(arr2));
-console.log(spiralTraverseR(arr3));
+// console.log(spiralTraverseR(arr1));
+// console.log(spiralTraverseR(arr2));
+// console.log(spiralTraverseR(arr3));
