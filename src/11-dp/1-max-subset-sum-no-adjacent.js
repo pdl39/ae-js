4a1c815a54742eed1eb034db027bf45d620a517e
pdl39 <dhlee9111@gmail.com>
2021-04-19 15:10:04 +0900

    [11-dp] ADD: problem 1

--- src/11-dp/1-max-subset-sum-no-adjacent.js
+++ src/11-dp/1-max-subset-sum-no-adjacent.js
@@ -1,6 +1,18 @@
-//
-//
+// T: O(n) | S: O(1)
+// where n = array length
 
 function maxSubsetSumNoAdjacent(array) {
-  
+  let prevMax = array[0];
+  let prev2Max = 0;
+
+  for (let i = 1; i < array.length; i++) {
+    const currentMax = Math.max(prevMax, prev2Max + array[i]);
+    prev2Max = prevMax;
+    prevMax = currentMax;
+  }
+
+  return prevMax;
 }
+
+
+console.log(maxSubsetSumNoAdjacent([1, 2, 3, 4, 5, 6, 7]));
