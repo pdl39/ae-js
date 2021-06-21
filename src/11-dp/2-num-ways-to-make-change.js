2dc9bc734e805e2de464ec39583d9def3a3650c2
pdl39 <dhlee9111@gmail.com>
2021-06-21 09:08:24 -0400

    [11-dp] ADD: problem 2

--- src/11-dp/2-num-ways-to-make-change.js
+++ src/11-dp/2-num-ways-to-make-change.js
@@ -1,6 +1,24 @@
-//
-//
+// T: O(nd) | S: O(n)
+// where n = target amount, d = number of denominations
 
 function numberOfWaysToMakeChange(n, denoms) {
-  // Write your code here.
+  const dp = new Array(n + 1).fill(0);
+  dp[0] = 1;
+
+  for (const denom of denoms) {
+    for (let amount = 1; amount < n + 1; amount++) {
+      if (amount >= denom) dp[amount] += dp[amount - denom];
+      console.log({denom, amount, dp});
+    }
+  }
+
+  return dp[n];
 }
+
+// TEST //
+const denoms1 = [1, 5, 10, 25];
+const denoms2 = [1, 2, 3];
+
+// console.log(numberOfWaysToMakeChange(10, denoms1));
+// console.log(numberOfWaysToMakeChange(5, denoms2));
+console.log(numberOfWaysToMakeChange(9, [3, 1]));
