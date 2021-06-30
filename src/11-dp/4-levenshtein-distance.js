5a74715b71900f4cf3bf78bf56e3009c59b19e81
pdl39 <dhlee9111@gmail.com>
2021-06-30 07:46:51 -0400

    [11-dp] UPDATE: problem 4 - added space optimized solution

--- src/11-dp/4-levenshtein-distance.js
+++ src/11-dp/4-levenshtein-distance.js
@@ -1,4 +1,39 @@
 // Using matrix (2-D array) to solve
+// Optimized for Space
+// T: O(n*m) | S: O(min(n, m))
+// where n = length of string 1, m = length of string 2 (or vice versa)
+
+function levenshteinDistance(str1, str2) {
+  const smallerStr = str2.length < str1.length ? str2 : str1;
+  const biggerStr = str2.length >= str1.length ? str2 : str1;
+
+  let currentRow = new Array(smallerStr.length + 1);
+  let prevRow = null;
+
+  for (let col = 0; col < currentRow.length; col++) {
+    currentRow[col] = col;
+  }
+
+  for (let row = 1; row < biggerStr.length + 1; row++) {
+    prevRow = currentRow;
+    currentRow = new Array(smallerStr.length + 1);
+    currentRow[0] = row;
+
+    for (let col = 1; col < currentRow.length; col++) {
+      if (biggerStr[row - 1] === smallerStr[col - 1]) {
+        currentRow[col] = prevRow[col - 1];
+      }
+      else {
+        currentRow[col] = 1 + Math.min(prevRow[col - 1], currentRow[col - 1], prevRow[col]);
+      }
+    }
+  }
+
+  return currentRow[currentRow.length - 1];
+}
+
+
+
 // T: O(n*m) | S: O(n*m) --> Space can be optimized to S: O(min(n, m))
 // where n = length of string 1, m = length of string 2 (or vice versa)
 
@@ -40,3 +75,4 @@ const str1 = 'abcde';
 const str2 = 'cfe';
 
 console.log(levenshteinDistance1(str1, str2));
+console.log(levenshteinDistance(str1, str2));
