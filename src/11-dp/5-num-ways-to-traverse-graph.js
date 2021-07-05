89e1a6e7ef36676e8b913f552346e59c853625f9
pdl39 <dhlee9111@gmail.com>
2021-07-05 00:47:24 -0400

    [11-dp] UPDATE: problem 5 - added a cleaner solution

--- src/11-dp/5-num-ways-to-traverse-graph.js
+++ src/11-dp/5-num-ways-to-traverse-graph.js
@@ -1,8 +1,52 @@
-// Use matrix (2-D array) and map path from start point to target
+// Use matrix (2-D array) and map get cummulative count for each slot (starting from source)
+// Optimized space complexity
+// T: O(w*h) | S: O(min(w, h));
+// where w = width, h = height
+
+function numberOfWaysToTraverseGraph3(width, height) {
+  const smallerSide = width < height ? width : height;
+  const biggerSide = width >= height ? widht : height;
+
+  //
+  const currentSide =
+
+}
+
+
+// Use matrix (2-D array) and map get cummulative count for each slot (starting from source)
+// Better implementation over starting from target using separte nodes to track count and neighbors for each.
 // T: O(w*h) | S: O(w*h)
 // where w = width, h = height
 
-function numberOfWaysToTraverseGraph(width, height) {
+function numberOfWaysToTraverseGraph2(width, height) {
+  // Create matrix
+  const matrix = new Array(height);
+  for (let i = 0; i < matrix.length; i++) {
+    matrix[i] = new Array(width).fill(0);
+  }
+
+  for (let wIdx = 0; wIdx < width; wIdx++) {
+    for (let hIdx = 0; hIdx < height; hIdx++) {
+      if (wIdx === 0 || hIdx === 0) {
+        matrix[wIdx][hIdx] = 1;
+      }
+      else {
+        const slotUp = matrix[wIdx][hIdx - 1];
+        const slotLeft = matrix[wIdx - 1][hIdx];
+        matrix[wIdx][hIdx] = slotUp + slotLeft;
+      }
+    }
+  }
+
+  return matrix[width - 1][height - 1];
+}
+
+
+// Use matrix (2-D array) and map path from start point to target (starting from target)
+// T: O(w*h) | S: O(w*h)
+// where w = width, h = height
+
+function numberOfWaysToTraverseGraph1(width, height) {
   // Create matrix
   const matrix = new Array(height);
   for (let i = 0; i < matrix.length; i++) {
@@ -51,5 +95,17 @@ const sumNeighborsCount = function(neighbors) {
 
 
 // TEST //
-console.log(numberOfWaysToTraverseGraph(2, 3));
-console.log(numberOfWaysToTraverseGraph(3, 4));
+console.log('numberOfWasToTraverGraph 1');
+console.log(numberOfWaysToTraverseGraph1(2, 3));
+console.log(numberOfWaysToTraverseGraph1(3, 4));
+console.log(numberOfWaysToTraverseGraph1(5, 6));
+
+console.log('numberOfWasToTraverGraph 2');
+console.log(numberOfWaysToTraverseGraph2(2, 3));
+console.log(numberOfWaysToTraverseGraph2(3, 4));
+console.log(numberOfWaysToTraverseGraph2(5, 6));
+
+console.log('numberOfWasToTraverGraph 3');
+console.log(numberOfWaysToTraverseGraph3(2, 3));
+console.log(numberOfWaysToTraverseGraph3(3, 4));
+console.log(numberOfWaysToTraverseGraph3(5, 6));
