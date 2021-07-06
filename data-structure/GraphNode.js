4714a33eca8007ac246322b6bb9aee6d2aa4f5d7
pdl39 <dhlee9111@gmail.com>
2021-07-06 07:10:18 -0400

    [data-structure] UPDATE: GraphNode - changed to a class of its own instead of extending the Node class - added some methods (incl. bfsWithAction, which still needs work)

--- data-structure/GraphNode.js
+++ data-structure/GraphNode.js
@@ -1,12 +1,59 @@
-import Node from './_Node';
+import Queue from './Queue';
 
-export default class GraphNode extends Node {
+export default class GraphNode{
   constructor(val) {
-    super(val);
+    this.value = val;
     this.edges = [];
   }
 
   addEdge(vertex) {
-    this.edges.push(vertex)
+    this.edges.push(vertex);
+  }
+
+  // bfsWithAction method is meant to be reusable across different actions to implement on each visit in a bfs traversal. Takes 2 required arguments: 1) action, which is a function that represents the action to implement on each visit, and 2) argValue, which is the intial value for the argNode, the first argument to the action function. It can also have additional arguments that represent any additional arguments for the action function, in addition to the argNode. argNode is used as the argument to the function to preserve the updated value during the traversal.
+  bfsWithAction(action, argValue, ...additionalArgs) {
+    if (!this) return;
+
+    const arg = new ArgNode(argValue);
+
+    const queue = new Queue();
+    queue.enque(this);
+
+    while (!queue.isEmpty()) {
+      const current = queue.dequeue();
+
+      if (current) {
+        action(arg, ...additionalArgs);
+        for (const edge of this.edges) {
+          this.bfs.enque(edge);
+        }
+      }
+    }
+
+    return arg.value;
+  }
+
+  getSize() {
+    return this.bfsWithAction(this.count, 0);
+  }
+
+  getBfsList() {
+    return this.bfsWithAction(this.list, []);
+  }
+
+  count(argNode) {
+    // argNode.value is a number
+    argNode.value++;
+  }
+
+  list(argNode, value) {
+    // argNode.value is an array
+    argNode.value.push(value);
+  }
+}
+
+class ArgNode {
+  constructor(val) {
+    this.value = val;
   }
 }
