71fc65a507a2efbb8cd79dd33cae1f5dfe67852f
pdl39 <dhlee9111@gmail.com>
2021-07-06 07:12:12 -0400

    [4-graphs] ADD: problem 4 (river size) - working

---
+++ src/4-graphs/4-river-sizes.js
@@ -0,0 +1,18 @@
+import GraphNode from "../../data-structure/GraphNode";
+
+// T: O() | S: O()
+// where
+
+function riverSizes(matrix) {
+  // Write your code here.
+}
+
+class River extends GraphNode {
+  constructor(val) {
+    super(val);
+  }
+
+  getSize() {
+
+  }
+}
