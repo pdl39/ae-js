09426a1727e844227e4cc70713df32f5481b6ade
pdl39 <dhlee9111@gmail.com>
2021-07-11 10:23:51 -0400

    [4-graphs] REFACTOR: problem 4 - removed unnecessary comments

--- src/4-graphs/4-river-sizes.js
+++ src/4-graphs/4-river-sizes.js
@@ -9,7 +9,7 @@ const River = require("../../data-structure/GraphNode");
 // - else, recurse through its edges, if edge is not seen and is 1. add edges to the seen map.
 //   --> each recursive call takes 1 work + max 4 recursive calls for each edge
 
-// max w*h space needed for the seen map.
+// max w*h space needed for the seen map (and for rivers array).
 
 
 function riverSizes(matrix) {
@@ -98,32 +98,3 @@ const matrix3 = [
 console.log(riverSizes(matrix1));
 console.log(riverSizes(matrix2));
 console.log(riverSizes(matrix3));
-
-
-
-
-
-
-
-
-// const river = new River('A');
-// const river1 = new River('B');
-// const river2 = new River('C');
-// const river3 = new River('D');
-// const river4 = new River('E');
-// const river5 = new River('F');
-// const river6 = new River('G');
-// const river7 = new River('H');
-// const river8 = new River('I');
-// const river9 = new River('J');
-// const river10 = new River('K');
-
-// river.addEdges(river1, river2);
-// river1.addEdges(river3, river4);
-// river2.addEdges(river5, river6);
-// river3.addEdges(river7, river8);
-// river4.addEdges(river9, river10);
-
-
-// console.log(river.getSize());
-// console.log(river.getBfsList());
