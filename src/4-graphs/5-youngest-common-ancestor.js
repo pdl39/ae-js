765e199f27f04c1d74299fb7100cff43f64dd2b4
pdl39 <dhlee9111@gmail.com>
2021-07-12 03:02:34 -0400

    [4-graphs] ADD: problem 5 (youngest common ancestor)

---
+++ src/4-graphs/5-youngest-common-ancestor.js
@@ -0,0 +1,77 @@
+// T: O() | S: O()
+// where
+
+class AncestralTree {
+  constructor(name) {
+    this.name = name;
+    this.ancestor = null;
+  }
+}
+
+function getYoungestCommonAncestor(topAncestor, descendantOne, descendantTwo) {
+  let currentNode = descendantOne;
+  let desc1Depth = 0, desc2Depth = 0;
+  while (currentNode !== topAncestor) {
+    desc1Depth++;
+    currentNode = currentNode.ancestor;
+  }
+
+  currentNode = descendantTwo;
+  while (currentNode !== topAncestor) {
+    desc2Depth++;
+    currentNode = currentNode.ancestor;
+  }
+
+  const biggerDepthDesc = desc1Depth >= desc2Depth ? {
+    node: descendantOne,
+    depth: desc1Depth
+  } : {
+    node: descendantTwo,
+    depth: desc2Depth
+  }
+
+  const smallerDepthDesc = desc1Depth < desc2Depth ? {
+    node: descendantOne,
+    depth: desc1Depth
+  } : {
+    node: descendantTwo,
+    depth: desc2Depth
+  }
+
+  while (biggerDepthDesc.depth > smallerDepthDesc.depth) {
+    biggerDepthDesc.node = biggerDepthDesc.node.ancestor;
+    biggerDepthDesc.depth--;
+  }
+
+  if (biggerDepthDesc.node === smallerDepthDesc.node) return biggerDepthDesc.node;
+
+  while (biggerDepthDesc.node.ancestor !== smallerDepthDesc.node.ancestor) {
+    biggerDepthDesc.node = biggerDepthDesc.node.ancestor;
+    smallerDepthDesc.node = smallerDepthDesc.node.ancestor;
+  }
+
+  return biggerDepthDesc.node.ancestor;
+}
+
+
+
+// TEST //
+const a = new AncestralTree('A');
+const b = new AncestralTree('B');
+const c = new AncestralTree('C');
+const d = new AncestralTree('D');
+const e = new AncestralTree('E');
+const f = new AncestralTree('F');
+const g = new AncestralTree('G');
+const h = new AncestralTree('H');
+const i = new AncestralTree('I');
+i.ancestor = d;
+h.ancestor = d;
+d.ancestor = b;
+e.ancestor = b;
+f.ancestor = c;
+g.ancestor = c;
+b.ancestor = a;
+c.ancestor = a;
+
+console.log(getYoungestCommonAncestor(a, e, i));
