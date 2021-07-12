bd33cbaeacde965cc345848c9624f2653a397829
pdl39 <dhlee9111@gmail.com>
2021-07-12 01:40:12 -0400

    [4-graphs] UPDATE: problem 4 - added a cleaner solution using matrix of equal size to keep track of visited nodes

--- src/4-graphs/4-river-sizes.js
+++ src/4-graphs/4-river-sizes.js
@@ -1,5 +1,67 @@
 const River = require("../../data-structure/GraphNode");
 
+// Using matrix of equal size to keep track of visited nodes
+// T: O(w*h) | S: O(w*h)
+// where w = matrix width, h = matrix height
+
+function riverSizes(matrix) {
+  const riverSizesArr = [];
+  const visited = new Array(matrix.length);
+  for (let i = 0; i < visited.length; i++) {
+    visited[i] = new Array(matrix[0].length).fill(false);
+  }
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[0].length; j++) {
+      if (matrix[i][j] === 0 || visited[i][j]) continue;
+
+      exploreRiver(i, j, matrix, visited, riverSizesArr);
+    }
+  }
+
+  return riverSizesArr;
+}
+
+function exploreRiver(i, j, matrix, visited, riverSizesArr) {
+  let currentRiverSize = 0;
+  let nodesToExplore = [[i, j]];
+
+  while (nodesToExplore.length) {
+    const currentNode = nodesToExplore.pop();
+    const i = currentNode[0];
+    const j = currentNode[1];
+
+    visited[i][j] = true;
+    currentRiverSize++;
+
+    const edges = getNonZeroEdges(currentNode, matrix);
+
+    edges.forEach(edge => {
+      const i = edge[0], j = edge[1];
+      if (!visited[i][j]) {
+        nodesToExplore.push(edge);
+        visited[i][j] = true;
+      }
+    });
+  }
+  riverSizesArr.push(currentRiverSize);
+}
+
+function getNonZeroEdges(currentNode, matrix) {
+  const i = currentNode[0];
+  const j = currentNode[1];
+
+  let left = matrix[i][j - 1] ? [i, j - 1] : null;
+  let right = matrix[i][j + 1] ? [i, j + 1] : null;
+  let up = matrix[i - 1] && matrix[i - 1][j] ? [i - 1, j] : null;
+  let down = matrix[i + 1] && matrix[i + 1][j] ? [i + 1, j] : null;
+
+  const edgesInclNull = [left, right, up, down];
+  return edgesInclNull.filter(edge => edge !== null);
+}
+
+
+
 // T: O(w*h) | S: O(w*h)
 // where w = matrix width, h = matrix height
 // we iterate through each slot of the matrix (w*h)
@@ -12,7 +74,7 @@ const River = require("../../data-structure/GraphNode");
 // max w*h space needed for the seen map (and for rivers array).
 
 
-function riverSizes(matrix) {
+function riverSizes1(matrix) {
   const matrixW = matrix[0].length;
   const matrixH = matrix.length;
 
@@ -94,6 +156,10 @@ const matrix3 = [
   [1, 0, 1, 0, 1],
   [1, 0, 1, 0, 1]
 ]
+
+console.log(riverSizes1(matrix1));
+console.log(riverSizes1(matrix2));
+console.log(riverSizes1(matrix3));
 
 console.log(riverSizes(matrix1));
 console.log(riverSizes(matrix2));
