1bea0e3ed1ee82d4fe6e116da5975a3685767bfa
pdl39 <dhlee9111@gmail.com>
2021-07-14 11:39:29 -0400

    [4-graphs] UPDATE: problem 8 - brute force solution addeed

--- src/4-graphs/8-minimum-passes-matrix.js
+++ src/4-graphs/8-minimum-passes-matrix.js
@@ -1,50 +1,56 @@
 /* eslint-disable no-unused-vars */
-// T: O() | S: O()
-// where
+// T: O(x*w*h) | S: O(w*h)
+// where w = matrix width, h = matrix height, x = number of passes
 
 function minimumPassesOfMatrix(matrix) {
   let passCount = 0;
-  let negatives = {count: 0};
-  const visited = matrix.map(row => row.map(value => 0));
+  let negatives = {count: 0, numConverted: 0, remaining: {}, convertedNegatives: {}};
 
   do {
     for (let i = 0; i < matrix.length; i++) {
       for (let j = 0; j < matrix[0].length; j++) {
         if (matrix[i][j] >= 0) continue;
 
-        const possibleReturn = convertToPositive(i, j, matrix, visited, negatives);
+        const possibleReturn = convertToPositive(i, j, matrix, negatives);
         if (possibleReturn === -1) return -1;
       }
     }
+    if (negatives.numConverted === 0) return 0;
     passCount++;
-    console.log(negatives.count);
-    negatives.count = 0;
+    negatives.convertedNegatives = {};
   } while (negatives.count > 0);
 
   return passCount;
 }
 
-function convertToPositive(i, j, matrix, visited, negatives) {
-  negatives.count++;
+function convertToPositive(i, j, matrix, negatives) {
+  if (!negatives.remaining[`${i}-${j}`]) negatives.count++;
   const edges = getEdges(i, j, matrix);
 
   let zeroCount = 0;
   for (const edge of edges) {
-    if (edge === 0) zeroCount++;
-    if (edge > 0) {
+    let edgeVal = matrix[edge[0]][edge[1]];
+    const edgeWasNotConverted = !negatives.convertedNegatives[`${edge[0]}-${edge[1]}`] && !negatives.convertedNegatives[`${i}-${j}`];
+
+    if (edgeVal === 0) zeroCount++;
+    if (edgeVal > 0 && edgeWasNotConverted) {
+      negatives.convertedNegatives[`${i}-${j}`] = matrix[i][j];
       matrix[i][j] *= -1;
-      console.log(matrix[i][j]);
+      negatives.numConverted++;
       negatives.count--;
+      delete negatives.remaining[`${i}-${j}`];
+      return;
     }
   }
+  negatives.remaining[`${i}-${j}`] = matrix[i][j];
   if (zeroCount === edges.length) return -1;
 }
 
 function getEdges(i, j, matrix) {
-  let left = matrix[i][j - 1] !== undefined ? matrix[i][j - 1] : null;
-  let right = matrix[i][j + 1] !== undefined ? matrix[i][j + 1] : null;
-  let up = matrix[i - 1] !== undefined ? matrix[i - 1][j] : null;
-  let down = matrix[i + 1] !== undefined ? matrix[i + 1][j] : null;
+  let left = matrix[i][j - 1] !== undefined ? [i, j - 1] : null;
+  let right = matrix[i][j + 1] !== undefined ? [i, j + 1] : null;
+  let up = matrix[i - 1] !== undefined ? [i - 1, j] : null;
+  let down = matrix[i + 1] !== undefined ? [i + 1, j] : null;
 
   return [left, right, up, down].filter(edge => edge !== null);
 }
