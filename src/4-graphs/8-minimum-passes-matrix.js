899f3e38e898dd0fe5e3abe94a50a69bce6a86b0
pdl39 <dhlee9111@gmail.com>
2021-07-17 09:41:40 -0400

    [4-graphs] UPDATE: problem 8 - betters solution added, but fails on edge cases. working..

--- src/4-graphs/8-minimum-passes-matrix.js
+++ src/4-graphs/8-minimum-passes-matrix.js
@@ -1,8 +1,75 @@
 /* eslint-disable no-unused-vars */
+const Queue = require('../../data-structure/Queue');
+
+// T: O(w*h) | S: O(w*h)
+// where w = matrix width, h = matrix height
+
+function minimumPassesOfMatrix(matrix) {
+  let passCount = 0;
+  const visited = matrix.map(row => row.map(value => false));
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[0].length; j++) {
+      if (matrix[i][j] > 0) {
+        const currentPassCount = bfsTraverse(i, j, matrix, visited);
+        passCount = Math.max(passCount, currentPassCount);
+      }
+    }
+  }
+
+  // one more iteration to check if any non-converted negatives remain
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[0].length; j++) {
+      if (matrix[i][j] < 0 && !visited[i][j]) {
+        return -1;
+      }
+    }
+  }
+
+  return passCount;
+}
+
+function bfsTraverse(i, j, matrix, visited) {
+  let currentLevelCount = 0;
+  const bfsQ = new Queue();
+  bfsQ.enque([i, j]);
+
+  while (!bfsQ.isEmpty()) {
+    const bfsQLen = bfsQ.length;
+    console.log({bfsQLen})
+
+    for (let n = 0; n < bfsQLen; n++) {
+      const currentSlot = bfsQ.dequeue().value;
+      console.log({currentSlot});
+      const i = currentSlot[0];
+      const j = currentSlot[1];
+      const unvisitedNegNeighbors = getUnvisitedNegNeighbors(i, j, matrix, visited);
+
+      for (const neighbor of unvisitedNegNeighbors) {
+        bfsQ.enque(neighbor);
+        visited[neighbor[0]][neighbor[1]] = true;
+      }
+    }
+    currentLevelCount++;
+    console.log({currentLevelCount})
+  }
+
+  return currentLevelCount - 1;
+}
+
+function getUnvisitedNegNeighbors(i, j, matrix, visited) {
+  let left = matrix[i][j - 1] < 0 && !visited[i][j - 1] ? [i, j - 1] : null;
+  let right = matrix[i][j + 1] < 0 && !visited[i][j + 1] ? [i, j + 1] : null;
+  let up = matrix[i - 1] && matrix[i - 1][j] < 0 && !visited[i - 1][j] ? [i - 1, j] : null;
+  let down = matrix[i + 1] && matrix[i + 1][j] < 0 && !visited[i + 1][j] ? [i + 1, j] : null;
+  return [left, right, up, down].filter(neighbor => neighbor !== null);
+}
+
+
 // T: O(x*w*h) | S: O(w*h)
 // where w = matrix width, h = matrix height, x = number of passes
 
-function minimumPassesOfMatrix(matrix) {
+function minimumPassesOfMatrix1(matrix) {
   let passCount = 0;
   let negatives = {count: 0, numConverted: 0, remaining: {}, convertedNegatives: {}};
 
@@ -62,5 +129,20 @@ const matrix1 = [
   [-5, 2, 0],
   [-6, -2, 0]
 ];
+const matrix2 = [
+  [0, -2, -1, 0],
+  [-5, 0, 0, 1],
+  [-6, -2, 1, 0]
+];
+const matrix3 = [
+  [ 1,  0,  0, -2, -3],
+  [-4, -5, -6, -2, -1],
+  [ 0,  0,  0,  0, -1],
+  [ 1,  2,  3,  0,  3]
+]
 
+// console.log(minimumPassesOfMatrix1(matrix1));
+// console.log(minimumPassesOfMatrix1(matrix2));
 console.log(minimumPassesOfMatrix(matrix1));
+console.log(minimumPassesOfMatrix(matrix2));
+console.log(minimumPassesOfMatrix(matrix3));
