33b794dcce29a345c290aa5049f98c49ba370667
pdl39 <dhlee9111@gmail.com>
2021-07-23 07:41:14 -0400

    [5-greedy] UDPATE: problem 4 - solution added

--- src/5-greedy-algorithm/4-task-assignment.js
+++ src/5-greedy-algorithm/4-task-assignment.js
@@ -1,13 +1,24 @@
-// T: O() | S: O()
-// where
+// T: O(nlogn) | S: O(n)
+// where n = tasks array length
 
 function taskAssignment(k, tasks) {
-  tasks.sort((a, b) => a - b);
-  return tasks;
+  if (k === 1) return [[0, 1]];
+
+  const resultingPairs = [];
+  const tasksObj = tasks.map((task, idx) => {return {duration: task, index: idx}});
+  tasksObj.sort((a, b) => a.duration - b.duration);
+
+  for (let i = 0, j = tasksObj.length - 1; i < j; i++, j--) {
+    resultingPairs.push([tasksObj[i].index, tasksObj[j].index]);
+  }
+
+  return resultingPairs;
 }
 
 
 // TEST //
 const tasks1 = [2, 1, 5, 4, 1, 3];
+const tasks2 = [1, 3, 5, 3, 1, 4];
 
 console.log(taskAssignment(3, tasks1));
+console.log(taskAssignment(3, tasks2));
