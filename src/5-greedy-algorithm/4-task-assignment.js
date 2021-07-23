8e7db011f03fa60cb484a1f3d70552f8103cf76a
pdl39 <dhlee9111@gmail.com>
2021-07-23 17:09:34 -0400

    [5-greedy] REFACTOR: problem 4 - changed the name of tasksObj to tasksObjArr

--- src/5-greedy-algorithm/4-task-assignment.js
+++ src/5-greedy-algorithm/4-task-assignment.js
@@ -5,11 +5,11 @@ function taskAssignment(k, tasks) {
   if (k === 1) return [[0, 1]];
 
   const resultingPairs = [];
-  const tasksObj = tasks.map((task, idx) => {return {duration: task, index: idx}});
-  tasksObj.sort((a, b) => a.duration - b.duration);
+  const tasksObjArr = tasks.map((task, idx) => {return {duration: task, index: idx}});
+  tasksObjArr.sort((a, b) => a.duration - b.duration);
 
-  for (let i = 0, j = tasksObj.length - 1; i < j; i++, j--) {
-    resultingPairs.push([tasksObj[i].index, tasksObj[j].index]);
+  for (let i = 0, j = tasksObjArr.length - 1; i < j; i++, j--) {
+    resultingPairs.push([tasksObjArr[i].index, tasksObjArr[j].index]);
   }
 
   return resultingPairs;
